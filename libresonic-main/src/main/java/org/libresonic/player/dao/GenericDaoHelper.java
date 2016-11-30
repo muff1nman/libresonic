e1366d8a5b2b507c6dad067f2c8362033a76bd89
Andrew DeMaria <lostonamountain@gmail.com>
2016-11-30 15:16:04 -0700

    wip

---
+++ libresonic-main/src/main/java/org/libresonic/player/dao/GenericDaoHelper.java
@@ -0,0 +1,24 @@
+package org.libresonic.player.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class GenericDaoHelper implements DaoHelper {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Override
+    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+        return namedParameterJdbcTemplate;
+    }
+}
