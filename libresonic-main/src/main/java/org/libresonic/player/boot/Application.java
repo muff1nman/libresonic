61d1388b43584f6503d4e4743e8746af9701db60
Andrew DeMaria <lostonamountain@gmail.com>
2017-01-03 22:23:21 -0700

    Save point 2

--- libresonic-main/src/main/java/org/libresonic/player/boot/Application.java
+++ libresonic-main/src/main/java/org/libresonic/player/boot/Application.java
@@ -1,18 +1,23 @@
 package org.libresonic.player.boot;
 
+import javax.servlet.Filter;
+import javax.servlet.ServletContextListener;
+import net.sf.ehcache.constructs.web.ShutdownListener;
 import org.directwebremoting.servlet.DwrServlet;
+import org.libresonic.player.filter.BootstrapVerificationFilter;
+import org.libresonic.player.filter.ParameterDecodingFilter;
+import org.libresonic.player.filter.RESTFilter;
+import org.libresonic.player.filter.RequestEncodingFilter;
+import org.libresonic.player.filter.ResponseHeaderFilter;
 import org.libresonic.player.spring.AdditionalPropertySourceConfigurer;
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-
-import static org.eclipse.persistence.expressions.ExpressionOperator.Add;
 
 @SpringBootApplication
 @Configuration
@@ -34,6 +39,110 @@ public class Application extends SpringBootServletInitializer {
         return servlet;
     }
 
+    @Bean
+    public ServletRegistrationBean cxfServletBean() {
+        return new ServletRegistrationBean(new org.apache.cxf.transport.servlet.CXFServlet(), "/ws/*");
+    }
+
+    @Bean
+    public ServletContextListener ehCacheShutdownListener() {
+        return new ShutdownListener();
+    }
+
+    @Bean
+    public FilterRegistrationBean bootstrapVerificationFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(bootstrapVerificationFiler());
+        registration.addUrlPatterns("/*");
+        registration.setName("BootstrapVerificationFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    @Bean
+    public Filter bootstrapVerificationFiler() {
+        return new BootstrapVerificationFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean parameterDecodingFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(parameterDecodingFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("ParameterDecodingFilter");
+        registration.setOrder(2);
+        return registration;
+    }
+
+    @Bean
+    public Filter parameterDecodingFilter() {
+        return new ParameterDecodingFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean restFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(restFilter());
+        registration.addUrlPatterns("/rest/*");
+        registration.setName("RESTFilter");
+        registration.setOrder(3);
+        return registration;
+    }
+
+    @Bean
+    public Filter restFilter() {
+        return new RESTFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean requestEncodingFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(requestEncodingFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("encoding", "UTF-8");
+        registration.setName("RequestEncodingFilter");
+        registration.setOrder(4);
+        return registration;
+    }
+
+    @Bean
+    public Filter requestEncodingFilter() {
+        return new RequestEncodingFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean cacheFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(cacheFilter());
+        registration.addUrlPatterns("/icons/*", "/style/*");
+        registration.addInitParameter("Cache-Control", "max-age=36000");
+        registration.setName("CacheFilter");
+        registration.setOrder(5);
+        return registration;
+    }
+
+    @Bean
+    public Filter cacheFilter() {
+        return new ResponseHeaderFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean noCacheFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(noCacheFilter());
+        registration.addUrlPatterns("/statusChart.view", "/userChart.view", "/playQueue.view", "/podcastChannels.view", "/podcastChannel.view", "/help.view", "/top.view", "/home.view");
+        registration.addInitParameter("Cache-Control", "no-cache, post-check=0, pre-check=0");
+        registration.addInitParameter("Pragma", "no-cache");
+        registration.addInitParameter("Expires", "Thu, 01 Dec 1994 16:00:00 GMT");
+        registration.setName("NoCacheFilter");
+        registration.setOrder(6);
+        return registration;
+    }
+
+    @Bean
+    public Filter noCacheFilter() {
+        return new ResponseHeaderFilter();
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
